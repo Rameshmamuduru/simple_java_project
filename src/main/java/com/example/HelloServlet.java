5b230bf5ebc62a36997a8c069a94417ddb42c3e1
Rameshmamuduru <rameshmamuduru7799@gmail.com>
2026-01-08 22:21:44 +0530

    Add files via upload

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h2>Hello from Servlet!</h2>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<head><title>Simple Web App</title></head>
+<body>
+<h1>Hello from Maven Web Application 🚀</h1>
+<p>Built with Maven and deployed on Tomcat.</p>
+<a href="hello">Call Servlet</a>
+</body>
+</html>
